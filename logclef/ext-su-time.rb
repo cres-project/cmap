a5aa1b81dacf0d3c7cb64694d5bd10dd9c2d1778
masao <masao@815a3dd3-8385-484b-ac9d-a4484e5f6727>
2010-07-08 10:14:30 +0000

    action_num: number of actions in each unit.

--- logclef/ext-su-time.rb
+++ logclef/ext-su-time.rb
@@ -1,33 +1,35 @@
 #!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
-# count su number in each session
+
+# count su number and time in each session
 
 require "./ext-session.rb"
 
 sessions = load_logdata
 
 sessions.keys.each do |sesid|
-	session = sessions[ sesid ]
-	units = []
-	cur_data = []
-	action_data = nil
-	session.each do |data|
-		cur_data << data
-		if /^search_/ =~ data[ 6 ]
-			if action_data
-				units << cur_data
-			end
-			cur_data = [ data ]
-			action_data = data
-		end
-	end
-	if action_data and not cur_data.empty?
-		units << cur_data
-	end
-	units.each do |unit|
-		last_time  = Time.parse( unit[ -1][12] )
-		first_time = Time.parse( unit[ 0 ][12] )
-		time = ( last_time - first_time ).to_f
-		puts [ sesid, unit[0][6], time ].join( "\t" )
-	end
+   session = sessions[ sesid ]
+   units = []
+   cur_data = []
+   action_data = nil
+   session.each do |data|
+      cur_data << data
+      if /^search_/ =~ data[ 6 ]
+         if action_data
+            units << cur_data
+         end
+         cur_data = [ data ]
+         action_data = data
+      end
+   end
+   if action_data and not cur_data.empty?
+      units << cur_data
+   end
+   units.each do |unit|
+      last_time  = Time.parse( unit[ -1][12] )
+      first_time = Time.parse( unit[ 0 ][12] )
+      time = ( last_time - first_time ).to_f
+      action_num = unit.size == 1 ? 1 : unit.size - 1
+      puts [ sesid, unit[0][6], action_num, time ].join( "\t" )
+   end
 end
