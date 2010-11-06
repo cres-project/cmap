26eec4ca5fc261a7285add748be03493b83c2ece
Masao Takaku <tmasao@acm.org>
2010-11-06 07:24:03 +0000

    add assert clause

--- qthoney/log_converter/test/test_start.rb
+++ qthoney/log_converter/test/test_start.rb
@@ -12,7 +12,12 @@ module QTHoney
       LOG_TEST1 = File.join( File.dirname( __FILE__ ), "test-1.log" )
       def test_start1
          open( LOG_TEST1 ) do |io|
-            logdata = Log2.new( io )
+            logdata = Log2.new( io ).convert
+            assert( logdata )
+            assert( logdata.size > 0 )
+            assert( logdata.first )
+            assert( logdata.first[ :action ] )
+            assert_equal( :start, logdata.first[ :action ] )
          end
       end
    end
