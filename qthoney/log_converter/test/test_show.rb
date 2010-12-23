a92adf4f8716b0948b80d2fda934ce1ed461d83c
masao <masao@815a3dd3-8385-484b-ac9d-a4484e5f6727>
2010-12-23 14:56:39 +0000

    add show action.

---
+++ qthoney/log_converter/test/test_show.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+# $Id$
+
+require 'test/unit'
+require 'ftools'
+
+$:.push File.join( File.dirname( __FILE__ ), ".." )
+require "converter.rb"
+
+module QTHoney
+   class TestShow < Test::Unit::TestCase
+      LOG_TEST = File.join( File.dirname( __FILE__ ), "test-12.log" )
+
+      def test_show
+         open( LOG_TEST ) do |io|
+            logdata = Log2.new( io ).convert
+            assert( logdata )
+            assert( logdata.size > 0 )
+            assert( logdata.first )
+
+            show_actions = logdata.select{|e| e[ :action ] == :show }
+            assert_equal( 2, show_actions.size )
+            show_actions.each do |action|
+               assert( action.key?( :timestamp ), :timestamp )
+               assert( action.key?( :tab_id ), "no tab_id at show action." )
+               assert( action.key?( :page_id ), "no page_id at show action." )
+               assert( action.key?( :url ), "no url at show action." )
+               assert( action.key?( :page_type ), "no page_type at show action." )
+               assert( action.key?( :title ), "no title at show action." )
+            end
+         end
+      end
+   end
+end
