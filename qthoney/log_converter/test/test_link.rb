5d2eb81d12e57f38dfc91b5f4ea52c9f7eaaff9a
masao <masao@815a3dd3-8385-484b-ac9d-a4484e5f6727>
2010-12-24 12:02:01 +0000

    search support at search-toolbar

--- qthoney/log_converter/test/test_link.rb
+++ qthoney/log_converter/test/test_link.rb
@@ -17,6 +17,7 @@ module QTHoney
             "test-14.log" => 1,
             "test-15.log" => 1,
             "test-16.log" => 1,
+            "test-17.log" => 1,
          }.each do |file, linkcount|
             open( File.join( File.dirname( __FILE__ ), file ) ) do |io|
                logdata = Log2.new( io ).convert
@@ -25,7 +26,8 @@ module QTHoney
                assert( logdata.first )
 
                link_actions = logdata.select{|e| e[ :action ] == :link }
-               assert_equal( linkcount, link_actions.size )
+               assert_equal( linkcount, link_actions.size,
+                             "#{ linkcount } link action[s] should be recorded for #{ file }." )
                link_actions.each do |action|
                   [ :timestamp, :tab_id, :page_id, :url, :page_type, :title,
                     :anchor_text, :target_url,
--- qthoney/log_converter/test/test_search.rb
+++ qthoney/log_converter/test/test_search.rb
@@ -9,23 +9,26 @@ require "converter.rb"
 
 module QTHoney
    class TestSearch < Test::Unit::TestCase
-      LOG_TEST = File.join( File.dirname( __FILE__ ), "test-17.log" )
 
       def test_search
-         open( LOG_TEST ) do |io|
-            logdata = Log2.new( io ).convert
-            assert( logdata )
-            assert( logdata.size > 0 )
-            assert( logdata.first )
-
-            search_actions = logdata.select{|e| e[ :action ] == :search }
-            assert_equal( 1, search_actions.size,
-                          "one single search action should be recorded." )
-            search_actions.each do |action|
-               [ :timestamp, :tab_id, :page_id, :url, :page_type, :title,
-                 # TODO: :target_page_id, :target_tab_id,
-               ].each do |attr|
-                  assert( action.key?( attr ), "no #{ attr } at search action." )
+         {  "test-17.log" => 1,
+            "test-18.log" => 1
+         }.each do |file, searchcount|
+            open( File.join( File.dirname( __FILE__ ), file ) ) do |io|
+               logdata = Log2.new( io ).convert
+               assert( logdata )
+               assert( logdata.size > 0 )
+               assert( logdata.first )
+               search_actions = logdata.select{|e| e[ :action ] == :search }
+               assert_equal( 1, search_actions.size,
+                             "one single search action should be recorded." )
+               search_actions.each do |action|
+                  [ :timestamp, :tab_id, :page_id, :url, :page_type, :title,
+                    :searchengine_label, :query, :serp_page,
+                    # TODO: :target_page_id, :target_tab_id,
+                  ].each do |attr|
+                     assert( action.key?( attr ), "no #{ attr } at search action." )
+                  end
                end
             end
          end
