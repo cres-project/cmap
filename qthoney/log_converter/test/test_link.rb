0960f226eaf9c7a4ddc3bed7534f17b5e12be920
masao <masao@815a3dd3-8385-484b-ac9d-a4484e5f6727>
2010-12-24 06:45:06 +0000

    link tests added

--- qthoney/log_converter/test/test_link.rb
+++ qthoney/log_converter/test/test_link.rb
@@ -9,23 +9,30 @@ require "converter.rb"
 
 module QTHoney
    class TestLink < Test::Unit::TestCase
-      LOG_TEST = File.join( File.dirname( __FILE__ ), "test-12.log" )
+      LOG_TEST = 
 
       def test_link
-         open( LOG_TEST ) do |io|
-            logdata = Log2.new( io ).convert
-            assert( logdata )
-            assert( logdata.size > 0 )
-            assert( logdata.first )
+         {  "test-12.log" => 1,
+            "test-13.log" => 3,
+            "test-14.log" => 1,
+            "test-15.log" => 1,
+            "test-16.log" => 1,
+         }.each do |file, linkcount|
+            open( File.join( File.dirname( __FILE__ ), file ) ) do |io|
+               logdata = Log2.new( io ).convert
+               assert( logdata )
+               assert( logdata.size > 0 )
+               assert( logdata.first )
 
-            link_actions = logdata.select{|e| e[ :action ] == :link }
-            assert_equal( 1, link_actions.size )
-            link_actions.each do |action|
-               [ :timestamp, :tab_id, :page_id, :url, :page_type, :title,
-                 :anchor_text, :target_url,
-                 # TODO: :target_page_id, :target_tab_id,
-               ].each do |attr|
-                  assert( action.key?( attr ), "no #{ attr } at link action." )
+               link_actions = logdata.select{|e| e[ :action ] == :link }
+               assert_equal( linkcount, link_actions.size )
+               link_actions.each do |action|
+                  [ :timestamp, :tab_id, :page_id, :url, :page_type, :title,
+                    :anchor_text, :target_url,
+                    # TODO: :target_page_id, :target_tab_id,
+                  ].each do |attr|
+                     assert( action.key?( attr ), "no #{ attr } at link action." )
+                  end
                end
             end
          end
