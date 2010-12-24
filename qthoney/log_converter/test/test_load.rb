f0aa5d727a99e5cd0200a4d626613d1385378697
masao <masao@815a3dd3-8385-484b-ac9d-a4484e5f6727>
2010-12-24 08:05:50 +0000

    change action supported.

--- qthoney/log_converter/test/test_load.rb
+++ qthoney/log_converter/test/test_load.rb
@@ -9,24 +9,25 @@ require "converter.rb"
 
 module QTHoney
    class TestLoad < Test::Unit::TestCase
-      LOG_TEST = File.join( File.dirname( __FILE__ ), "test-12.log" )
-
       def test_load
-         open( LOG_TEST ) do |io|
-            logdata = Log2.new( io ).convert
-            assert( logdata )
-            assert( logdata.size > 0 )
-            assert( logdata.first )
-
-            load_actions = logdata.select{|e| e[ :action ] == :load }
-            assert_equal( 2, load_actions.size )
-            load_actions.each do |action|
-               assert( action.key?( :timestamp ) )
-               assert( action.key?( :tab_id ) )
-               assert( action.key?( :page_id ) )
-               assert( action.key?( :url ) )
-               assert( action.key?( :page_type ) )
-               assert( action.key?( :title ) )
+         {  "test-12.log" => 1,
+            "test-14.log" => 1,
+         }.each do |file, linkcount|
+            open( File.join( File.dirname( __FILE__ ), file ) ) do |io|
+               logdata = Log2.new( io ).convert
+               assert( logdata )
+               assert( logdata.size > 0 )
+               assert( logdata.first )
+               load_actions = logdata.select{|e| e[ :action ] == :load }
+               assert_equal( 2, load_actions.size )
+               load_actions.each do |action|
+                  assert( action.key?( :timestamp ) )
+                  assert( action.key?( :tab_id ) )
+                  assert( action.key?( :page_id ) )
+                  assert( action.key?( :url ) )
+                  assert( action.key?( :page_type ) )
+                  assert( action.key?( :title ) )
+               end
             end
          end
       end
