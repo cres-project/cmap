94a19e5d039713e29578fadb0babee5a35e69f9b
Masao Takaku <tmasao@acm.org>
2014-09-23 15:55:32 +0900

    add distances for each node.

--- ext-graph-pairs.rb
+++ ext-graph-pairs.rb
@@ -6,13 +6,16 @@ require "graph.rb"
 
 class DirectedGraph
   def to_node_pairs( name = nil )
-    str = %W[ Cmap node1 node2 link_label category category_node1 category_node2 ].join( "\t" )
+    count = self.distance_count
+    str = %W[ Cmap dist_node1 dist_node2
+              node1 node2 link_label category category_node1 category_node2 ].join( "\t" )
     str << "\n"
     self.each do |n1|
       next if edges_to[n1].nil?
       edges_to[n1].each do |n2|
         edge_label = edge_labels[ Set[n1,n2] ]
-        str << [ name, node_labels[n1], node_labels[n2], edge_label, ].join( "\t" )
+        str << [ name, @dists[Set["id0", n1]], @dists[Set["id0", n2]],
+	         node_labels[n1], node_labels[n2], edge_label, ].join( "\t" )
 	str << "\n"
       end
     end
--- test/test_ext-graph-pairs.rb
+++ test/test_ext-graph-pairs.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/env ruby
 
 require 'test/unit'
-require 'ftools'
 
 require "stringio"
 
@@ -16,13 +15,24 @@ class TestExtGraphPairs < Test::Unit::TestCase
       g = DirectedGraph.load_dot2( open(post_fname) )
       results = g.to_node_pairs
       strio = StringIO.new( results )
-      header = strio.gets # skip header
+      header = strio.gets
       header_a = header.chomp.split( /\t/ )
       assert_equal( header_a[0], "Cmap" )
 
       data0 = strio.gets.chomp.split( /\t/ )
-      assert_equal( data0, ["", "Democratic Party", "Hatoyama", "conduct" ] )
-      #test-0-post     Democratic Party        Hatoyama        conduct
-      #test-0-post     Democratic Party        Effect of a change of government        make
+      assert_equal( data0, ["", "1", "0", "Democratic Party", "Effect of a change of government", "make" ] )
+      data1 = strio.gets.chomp.split( /\t/ )
+      assert_equal( data1, ["", "1", "2", "Democratic Party", "Hatoyama", "conduct" ] )
+      data2 = strio.gets.chomp.split( /\t/ )
+      assert_equal( data2, ["", "2", "3", "Hatoyama", "Mrs. Miyuki Hatoyama", "with" ] )
+
+      pre_fname = File.join( BASEDIR, "test-0-pre.dot")
+      g = DirectedGraph.load_dot2( open(pre_fname) )
+      results = g.to_node_pairs
+      strio = StringIO.new( results )
+      strio.gets # skip header
+      data0 = strio.gets.chomp.split( /\t/ )
+      assert_equal( data0, [ "", "4", "5", "Akihabara", "electric city", "is" ] )
+
    end
 end
